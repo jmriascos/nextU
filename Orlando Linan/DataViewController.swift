348726ca203ff06f22686963c36d0b945cd2a2d8
Jorge Mario Riascos Uribe <jmriascos@jorges-macbook-pro.local>
2017-01-13 21:44:45 -0500

    Initial Commit

---
+++ Orlando Linan/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Orlando Linan
+//
+//  Created by Jorge Mario Riascos Uribe on 1/13/17.
+//  Copyright © 2017 Jorge Mario Riascos Uribe. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
